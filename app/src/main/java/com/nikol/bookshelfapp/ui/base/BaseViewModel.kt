c66f4c493c39f75e0f5f95e76a5ee4b8aa60442e
Nikita Travin <travin.nikita@gmail.com>
2021-08-15 15:03:50 +0600

    add singleLiveEvent class for loading animation

--- app/src/main/java/com/nikol/bookshelfapp/ui/base/BaseViewModel.kt
+++ app/src/main/java/com/nikol/bookshelfapp/ui/base/BaseViewModel.kt
@@ -1,11 +1,14 @@
 package com.nikol.bookshelfapp.ui.base
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import org.koin.core.KoinComponent
 
 abstract class BaseViewModel: ViewModel(), KoinComponent {
     protected val compositeDisposable = CompositeDisposable()
+
+    val isLoading = MutableLiveData(false)
 
     override fun onCleared() {
         compositeDisposable.clear()
