1bb15911f94bf98ed39b341da65e5cc2adb00662
Nikita Travin <travin.nikita@gmail.com>
2021-08-15 16:44:10 +0600

    iport safelet extension, add itemDecoration

--- app/src/main/java/com/nikol/bookshelfapp/ui/search/SearchFragment.kt
+++ app/src/main/java/com/nikol/bookshelfapp/ui/search/SearchFragment.kt
@@ -2,11 +2,17 @@ package com.nikol.bookshelfapp.ui.search
 
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.appcompat.widget.SearchView
+import androidx.core.graphics.drawable.DrawableCompat
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.nikol.bookshelfapp.R
 import com.nikol.bookshelfapp.databinding.MainFragmentBinding
 import com.nikol.bookshelfapp.ui.search.adapter.BooksSearchAdapter
+import com.nikol.bookshelfapp.utils.safeLet
 import org.koin.android.ext.android.inject
 
 class SearchFragment : Fragment() {
@@ -28,6 +34,8 @@ class SearchFragment : Fragment() {
         binding.lifecycleOwner = viewLifecycleOwner
         binding.viewModel = viewModel
         binding.recyclerViewBooks.adapter = booksSearchAdapter
+
+        addItemDecoration(binding.recyclerViewBooks)
         return binding.root
     }
 
@@ -61,6 +69,18 @@ class SearchFragment : Fragment() {
                 return true
             }
         })
+    }
+
+    private fun addItemDecoration(recyclerView: RecyclerView) {
+        val orientation = (recyclerView.layoutManager as? LinearLayoutManager)?.orientation
+        val divider =
+            AppCompatResources.getDrawable(recyclerView.context, R.drawable.layout_divider)
+
+        safeLet(orientation, divider) { orientation, divider ->
+            val itemDecoration = DividerItemDecoration(recyclerView.context, orientation)
+            itemDecoration.setDrawable(divider)
+            recyclerView.addItemDecoration(itemDecoration)
+        }
     }
 
 }
